cf24cd8f3870491cdac5d59041a2e95973e65151
Shubham Singh <myselfsinghshubham@gmail.com>
2025-09-16 20:33:44 +0000

    remove login from navbar

--- src/components/layout/user-nav.tsx
+++ src/components/layout/user-nav.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useEffect, useState } from 'react';
@@ -31,11 +32,7 @@ export function UserNav() {
   };
 
   if (!user) {
-    return (
-      <Button asChild>
-        <Link href="/login">Login</Link>
-      </Button>
-    );
+    return null;
   }
 
   return (
